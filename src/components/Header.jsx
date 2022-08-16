ecc80d4f597881b260f694312869dacf8672e7ad
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-16 14:07:33 -0300

    criação do componente button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-// import Button from "./Button"
+import { But } from "./Button"
 
 export const Head = () => {
     return(
       <header>
-         <p class="header-text-top">uma seleção de produtos</p>
-         <h1 class="header-title">especial para você</h1>
-         <p class="header-text-bottom">todos os produtos dessa lista foram selecionados a partir da sua navegação. Aproveite!</p>
+        <p class="header-text-top">uma seleção de produtos</p>
+        <h1 class="header-title">especial para você</h1>
+        <p class="header-text-bottom">todos os produtos dessa lista foram selecionados a partir da sua navegação. Aproveite!</p>
       
-         {/* <Button txt="Conheça a Linx"/>
-         <Button txt="Ajude o algorítimo"/>
-         <Button txt="Seus produtos"/>
-         <Button txt="Compartilhe"/>  */}
+        <div>
+            <But txt="Conheça a Linx"/>
+            <But txt="Ajude o algorítimo"/>
+            <But txt="Seus produtos"/>
+            <But txt="Compartilhe"/> 
+        </div>
       </header>
     )
-}   
-
-// export default Head
+}
