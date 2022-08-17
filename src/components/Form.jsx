f3c3559ee2f790ad3b958fb56793a5b360ce5b4c
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-17 11:11:07 -0300

    Uso de flexbox para organização do componente Form e separação do formTxt e formBox

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,40 @@
-
+import './css/form.css'
+import { But } from "./Button"
 
 export const Forms = () => {
     return (
         <form className="form">
 
-            <div>
+            <div className='formTxt'>
                 <h3>
                     Ajude o algorítimo a ser mais certeiro
                 </h3>
-                <p>um texto qualquer 1</p>
-                <p>um texto qualquer 2</p>
+                <p>um texto qualquer 1 - precisa ficar mais longo do que está agora</p>
+                <p>um texto qualquer 2 - para dar a impressão de pagágrafo</p>
             </div>
 
-            <div>
-                <label for="name">Seu nome</label>
+            <div className='formBox'>
+                <label for="name">Seu nome:</label>
                 <input type="text" name="name" placeholder="Digite seu nome" />
 
-                <label for="email">Seu E-mail</label>
+                <label for="email">E-mail:</label>
                 <input type="text" name="email" placeholder="Digite seu E-mail" />
                 
-                <label for="cpf">Seu CPF</label>
+                <label for="cpf">CPF:</label>
                 <input type="text" name="cpf" placeholder="Digite seu CPF" />
                 
-                <label for="gender">Feminino</label>
-                <input type="radio" value="feminino" name="gender" />
+                <div>
+                    <input type="radio" value="feminino" name="gender" />
+                    <label for="gender">Feminino</label>
                 
-                <label for="gender">Masculino</label>
-                <input type="radio" value="masculino" name="gender" />
+                    <input type="radio" value="masculino" name="gender" />
+                    <label for="gender">Masculino</label>
+                </div>
+
+                <But txt="Enviar"/>
+            
             </div>
+
             
         </form>
     )
