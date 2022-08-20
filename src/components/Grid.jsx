7f16e718352e8e3c11e5a2d09a07f68d718e170f
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-20 13:58:43 -0300

    Criação das requisições no Grid, para carregar os produtos na página

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,11 +1,64 @@
 import { Cards } from "./Card";
-import { But } from "./Button";
+// import { But } from "./Button";
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
-export const Grid = () => {
+
+function Grid() {
+
+    const [cardInfo, setCardInfo] = useState([]);
+    const [newUrl, setNewUrl] = useState("")
+
+    
+    useEffect(() => {
+        
+        axios.get("http://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1")
+        .then((response) =>{
+            setCardInfo([...response.data.products])
+            setNewUrl(response.data.nextPage);
+        })
+        .catch(function(error){
+            console.log(error)
+        });
+    }, [])
+    
+    const loadMore = () => {
+        axios.get(`http://${newUrl}`)
+        .then((response) => {
+            setCardInfo([...cardInfo, ...response.data.products])
+            setNewUrl(response.data.nextPage);
+        }) 
+        
+        .catch(function(error){
+            console.log(error)
+        });
+    }
+
     return (
-        <div className="grid">
-            <Cards />
-            <But txt="Ainda mais produtos aqui!" />
+        <div>
+            <div>
+                <h2>Sua Seleção Especial</h2> 
+            </div>
+            <div>
+            {cardInfo.map((card) => (
+                <Cards
+                    key={card.id}
+                    image={card.image}
+                    title={card.name}
+                    description={card.description}
+                    oldPrice={card.oldPrice}
+                    price={card.price}
+                    installmentsCount={card.installments.count}
+                    installmentsValue={card.installments.value}
+                />
+            ))}
+            </div>
+            <div>
+                {/* <But onClick={() => {}} txt="ainda mais aqui" /> */}
+                <button onClick={loadMore}>Ainda mais produtos aqui!</button>
+            </div>
         </div>
     )
 }
+
+export default Grid
